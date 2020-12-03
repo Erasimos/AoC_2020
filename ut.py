64146347f2c7271ce72462a80e94af92019707e6
Erasimos <rasmus.tomasson@gmail.com>
2020-12-03 09:59:02 +0100

    added tuple add method and read input method in ut

--- ut.py
+++ ut.py
@@ -6,10 +6,14 @@ def read_file(path):
     return contents
 
 
-def read_file_lines(path):
-    file = open(path, "r")
-    contents = file.readlines()
+def read_input():
+    file = open('input.txt', "r")
+    contents = file.read()
     return contents
+
+
+def pos_add(pos_1, pos_2):
+    return pos_1[0] + pos_2[0], pos_1[1] + pos_2[1]
 
 
 def print_answer(answer):
