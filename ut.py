f2beab376241407d06e862afb034f029184c2535
Erasimos <rasmus.tomasson@gmail.com>
2020-12-17 21:23:15 +0100

    added n-dimensional pos addition and lcm method in utilities

--- ut.py
+++ ut.py
@@ -1,4 +1,4 @@
-
+import math
 
 def read_file(path):
     file = open(path, "r")
@@ -13,7 +13,32 @@ def read_input():
 
 
 def pos_add(pos_1, pos_2):
-    return pos_1[0] + pos_2[0], pos_1[1] + pos_2[1]
+    new_pos = []
+    for i in range(len(pos_1)):
+        new_pos.append(pos_1[i] + pos_2[i])
+    return tuple(new_pos)
+
+
+def rotate_point_around_point(origin, point, angle):
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return qx, qy
+
+
+def pos_mul(pos, factor):
+    new_x = pos[0] * factor
+    new_y = pos[1] * factor
+    return new_x, new_y
+
+
+def lcm(numbers):
+    lcm = numbers[0]
+    for i in numbers[1:]:
+        lcm = lcm * i // math.gcd(lcm, i)
+    return lcm
 
 
 def print_answer(answer):
